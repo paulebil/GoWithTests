2983d93d651e5d0d813dfb688c612e3a9b43d2e6
paulebil <ebilpaul06@gmail.com>
2025-07-12 14:08:16 +0300

    wrote hello world test and an integer test

---
+++ 1helloworld/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+const (
+	spanish = "Spanish"
+	french = "French" 
+
+	englishHelloPrefix = "Hello, "
+    spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix = "Bonjour, "
+)
+
+
+func Hello(name string, language string) string{
+	if name == ""{
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+
+
+func greetingPrefix(language string) (prefix string){
+	// using a named return (prefix string)
+	// This will create a variable called `prefix` in your function
+	// It will be assigned the `zero` value. This depends on the type,
+	//  for example `int`s are ) and for `string`s it is ""
+
+	switch language{
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	// You can return whaterver it's set to by just calling `return` rather than `return prefix`
+	return
+}
+
+
+func main(){
+	fmt.Println(Hello("world", ""))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/paulebil/GoWithTests
+
+go 1.24.3
