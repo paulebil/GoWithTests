0b0ec40a7668ef8d9be47e84bbe1942988ae7861
paulebil <ebilpaul06@gmail.com>
2025-07-14 13:15:33 +0300

    finaly wraped the arrays and slices

--- 4ArraysSlices/sum_test.go
+++ 4ArraysSlices/sum_test.go
@@ -48,3 +48,43 @@ func TestSumAll(t *testing.T) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
+
+
+func TestSumAllTails(t *testing.T){
+
+	checkSums := func (t testing.TB, got, want []int)  {
+		t.Helper()
+
+		if !reflect.DeepEqual(got, want){
+			t.Errorf("got %v want %v", got , want)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1,2}, []int{0,9})
+		want := []int{2,9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+	
+}
+
+// We could've created a new function checkSums like we normally do, 
+// but in this case, we're showing a new technique, assigning a function to a variable.
+//  It might look strange but, it's no different to assigning a variable to a string, or an int,
+//  functions in effect are values too.
+
+// It's not shown here, but this technique can be useful when you want to bind a function to other 
+// local variables in "scope" (e.g between some {}). It also allows you to reduce the surface area 
+// of your API.
+
+// By defining this function inside the test, it cannot be used by other functions in this package.
+//  Hiding variables and functions that don't need to be exported is an important design consideration.
+
+// A handy side-effect of this is this adds a little type-safety to our code. If a developer mistakenly
+//  adds a new test with checkSums(t, got, "dave") the compiler will stop them in their tracks.
